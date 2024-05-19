92db40152dcb21002fd6c7a268549330f4280c85
monjara <yaginumee@gmail.com>
2024-05-19 13:41:22 +0900

    update

--- lua/floating-help/init.lua
+++ lua/floating-help/init.lua
@@ -4,9 +4,9 @@ M.createFloatingHelp = function(q)
   local lines = vim.o.lines - vim.o.cmdheight
   local columns = vim.o.columns
   local height = lines - 4
-  local width = math.min(columns - 10, 80)
-  local row = math.floor((lines - height) / 2)
-  local col = math.floor((columns - width) / 2)
+  local width = columns
+  local row = lines
+  local col = 0
 
   local opts = {
     height = height,
@@ -16,22 +16,20 @@ M.createFloatingHelp = function(q)
     relative = 'editor',
     style = 'minimal',
     zindex = 45,
-    border = 'single'
+    border = 'single',
   }
 
   local win_id = 0
-  local status, result = pcall(
-    function()
-      local bufnr = vim.api.nvim_create_buf(false, true)
-      win_id = vim.api.nvim_open_win(bufnr, true, opts)
-      vim.opt_local.filetype = 'help'
-      vim.opt_local.buftype = 'help'
-      vim.api.nvim_command('help ' .. q['args'])
+  local status, result = pcall(function()
+    local bufnr = vim.api.nvim_create_buf(false, true)
+    win_id = vim.api.nvim_open_win(bufnr, true, opts)
+    vim.opt_local.filetype = 'help'
+    vim.opt_local.buftype = 'help'
+    vim.api.nvim_command('help ' .. q['args'])
 
-      vim.keymap.set('n', 'q', '<cmd>q<cr>', { noremap = true, buffer = bufnr })
-      vim.keymap.set('n', '<esc>', '<cmd>q<cr>', { noremap = true, buffer = bufnr })
-    end
-  )
+    vim.keymap.set('n', 'q', '<cmd>q<cr>', { noremap = true, buffer = bufnr })
+    vim.keymap.set('n', '<esc>', '<cmd>q<cr>', { noremap = true, buffer = bufnr })
+  end)
   if not status then
     vim.api.nvim_win_close(win_id, true)
     return
--- plugin/floating-help.lua
+++ plugin/floating-help.lua
@@ -1,10 +1,6 @@
-vim.api.nvim_create_user_command(
-  'H',
-  function(opts)
-    require('floating-help').createFloatingHelp(opts)
-  end,
-  {
-    nargs = 1,
-    complete = 'help'
-  }
-)
+vim.api.nvim_create_user_command('H', function(opts)
+  require('floating-help').createFloatingHelp(opts)
+end, {
+  nargs = 1,
+  complete = 'help',
+})
